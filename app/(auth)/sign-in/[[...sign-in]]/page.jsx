a4bd8cf5728039230d21da9245ef9f0a9631e113
Yashini13 <yashupar81@gmail.com>
2025-05-31 18:14:31 +0530

    Changes in sign in

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,5 @@
 import { SignIn } from '@clerk/nextjs'
-
+import {img} from '../../WelCOME.png'
 export default function Page() {
   return(
   <section className="bg-white">
@@ -7,7 +7,7 @@ export default function Page() {
     <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
       <img
         alt=""
-        src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        src={img}
         className="absolute inset-0 h-full w-full object-cover opacity-80"
       />
 
@@ -28,11 +28,11 @@ export default function Page() {
         </a>
 
         <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-          Welcome to AI mock interview App ✨
+          Welcome to PrepWise AI  ✨
         </h2>
 
         <p className="mt-4 leading-relaxed text-white/90">
-        Prepare for Interviews online
+        Prepare for Interviews with AI Personalization
         </p>
       </div>
     </section>
@@ -47,10 +47,9 @@ export default function Page() {
   href="#"
 >
   <span className="sr-only">Home</span>
-  <img src="https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvdXBsb2FkZWQvaW1nXzJwd2JIM1BIdWFRREI1U1d4SXpPTHJXTWU2eSJ9?width=200" alt="Home" />
 </a>
           <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-            Welcome to MockMate AI
+            Welcome to PrepWise AI
           </h1>
 
           <p className="mt-4 leading-relaxed text-gray-500">
